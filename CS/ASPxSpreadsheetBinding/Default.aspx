a855bfe9b6d57b744c26674a80f01adc1180f997
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-20 02:47:26 +0300

    initial commit

---
+++ CS/ASPxSpreadsheetBinding/Default.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ASPxSpreadsheetBinding.Default" %>
+<%@ Register Assembly="DevExpress.Web.v17.2, Version=17.2.7.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a" Namespace="DevExpress.Web" TagPrefix="dx" %>
+<%@ Register Assembly="DevExpress.Web.ASPxSpreadsheet.v17.2, Version=17.2.7.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a" Namespace="DevExpress.Web.ASPxSpreadsheet" TagPrefix="dx" %>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script>
+        function onCustomCommandExecuted(s,e) {
+            if (e.commandName == "Download") {
+                __doPostBack("DownloadExcel");
+            }
+        }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <dx:ASPxSpreadsheet ID="Spreadsheet" runat="server" >
+            <ClientSideEvents CustomCommandExecuted="onCustomCommandExecuted" />
+        </dx:ASPxSpreadsheet>
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:DocumentsConnectionString %>" 
+            SelectCommand="SELECT * FROM [Docs]">
+        </asp:SqlDataSource>
+    </form>
+</body>
+</html>
---
+++ CS/ASPxSpreadsheetBinding/Default.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Data;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DevExpress.Spreadsheet;
+using DevExpress.Web.Office;
+using DevExpress.Web;
+using System.IO;
+
+namespace ASPxSpreadsheetBinding {
+    public partial class Default : System.Web.UI.Page {
+        string SessionKey = "EditedDocuemntID";
+        protected string EditedDocuemntID {
+            get { return (string)Session[SessionKey] ?? string.Empty; }
+            set { Session[SessionKey] = value; }
+        }
+        protected void Page_Init(object sender, EventArgs e) {
+            if (!IsPostBack) {
+                SetupSpreadsheetRibbon();
+                if (!string.IsNullOrEmpty(EditedDocuemntID))
+                    CloseSpreadsheetDocument();
+                OpenDocumentFromDatabase();
+            }
+            ExportExcelDocument();
+        }
+
+        void ExportExcelDocument() {
+            if (Request["__EVENTTARGET"] == "DownloadExcel") {
+                using (var stream = new MemoryStream()) {
+                    Spreadsheet.Document.SaveDocument(stream, DocumentFormat.Xlsx);
+                    Response.Clear();
+                    Response.ContentType = "application/application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                    Response.AddHeader("Content-Disposition", "attachment; filename=document.xlsx");
+                    Response.AddHeader("Content-Length", stream.Length.ToString());
+                    Response.BinaryWrite(stream.ToArray());
+                    Response.Flush();
+                    Response.Close();
+                    Response.End();
+                }
+            }
+        }
+        void SetupSpreadsheetRibbon() {
+            Spreadsheet.CreateDefaultRibbonTabs(true);
+            Spreadsheet.RibbonTabs.Find(p => p.Text == "File").Visible = false;
+            RibbonButtonItem item = new RibbonButtonItem("Download", "Download");
+            item.Size = RibbonItemSize.Large;
+            item.LargeImage.IconID = DevExpress.Web.ASPxThemes.IconID.ActionsDownload32x32;
+            Spreadsheet.RibbonTabs[1].Groups[0].Items.Insert(0, item);
+        }
+
+        #region open/close documents from a database
+        void OpenDocumentFromDatabase() {
+            EditedDocuemntID = Guid.NewGuid().ToString();
+            DataView view = (DataView)SqlDataSource1.Select(DataSourceSelectArguments.Empty);
+            Spreadsheet.Open(
+                EditedDocuemntID,
+                DocumentFormat.Xlsx,
+                () => { return (byte[])view.Table.Rows[0]["DocBytes"]; }
+            );
+        }
+
+        void CloseSpreadsheetDocument() {
+            DocumentManager.CloseDocument(DocumentManager.FindDocument(EditedDocuemntID).DocumentId);
+            EditedDocuemntID = string.Empty;
+        }
+        #endregion
+
+    }
+}
