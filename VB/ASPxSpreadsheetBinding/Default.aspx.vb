ca9bb4219bbeefcbe1c760d755acc3cde41b338d
DevExpressExampleBot <fakeEmail>
2026-01-30 11:49:31 +0400

    Source auto update [skip ci]

--- VB/ASPxSpreadsheetBinding/Default.aspx.vb
+++ VB/ASPxSpreadsheetBinding/Default.aspx.vb
@@ -1,4 +1,4 @@
-﻿Imports System
+Imports System
 Imports System.Data
 Imports System.Web.UI
 Imports System.Web.UI.WebControls
@@ -8,31 +8,34 @@ Imports DevExpress.Web
 Imports System.IO
 
 Namespace ASPxSpreadsheetBinding
-    Partial Public Class [Default]
-        Inherits System.Web.UI.Page
+
+    Public Partial Class [Default]
+        Inherits Page
 
         Private SessionKey As String = "EditedDocuemntID"
-        Protected Property EditedDocuemntID() As String
+
+        Protected Property EditedDocuemntID As String
             Get
-                Return If(DirectCast(Session(SessionKey), String), String.Empty)
+                Return If(CStr(Session(SessionKey)), String.Empty)
             End Get
+
             Set(ByVal value As String)
                 Session(SessionKey) = value
             End Set
         End Property
+
         Protected Sub Page_Init(ByVal sender As Object, ByVal e As EventArgs)
             If Not IsPostBack Then
                 SetupSpreadsheetRibbon()
-                If Not String.IsNullOrEmpty(EditedDocuemntID) Then
-                    CloseSpreadsheetDocument()
-                End If
+                If Not String.IsNullOrEmpty(EditedDocuemntID) Then CloseSpreadsheetDocument()
                 OpenDocumentFromDatabase()
             End If
+
             ExportExcelDocument()
         End Sub
 
         Private Sub ExportExcelDocument()
-            If Request("__EVENTTARGET") = "DownloadExcel" Then
+            If Equals(Request("__EVENTTARGET"), "DownloadExcel") Then
                 Using stream = New MemoryStream()
                     Spreadsheet.Document.SaveDocument(stream, DocumentFormat.Xlsx)
                     Response.Clear()
@@ -46,29 +49,27 @@ Namespace ASPxSpreadsheetBinding
                 End Using
             End If
         End Sub
+
         Private Sub SetupSpreadsheetRibbon()
             Spreadsheet.CreateDefaultRibbonTabs(True)
-            Spreadsheet.RibbonTabs.Find(Function(p) p.Text = "File").Visible = False
-            Dim item As New RibbonButtonItem("Download", "Download")
+            Spreadsheet.RibbonTabs.Find(CType(Function(p) Equals(p.Text, "File"), Predicate(Of RibbonTab))).Visible = False
+            Dim item As RibbonButtonItem = New RibbonButtonItem("Download", "Download")
             item.Size = RibbonItemSize.Large
-            item.LargeImage.IconID = DevExpress.Web.ASPxThemes.IconID.ActionsDownload32x32
+            item.LargeImage.IconID = ASPxThemes.IconID.ActionsDownload32x32
             Spreadsheet.RibbonTabs(1).Groups(0).Items.Insert(0, item)
         End Sub
 
-        #Region "open/close documents from a database"
+#Region "open/close documents from a database"
         Private Sub OpenDocumentFromDatabase()
             EditedDocuemntID = Guid.NewGuid().ToString()
-            Dim view As DataView = DirectCast(SqlDataSource1.Select(DataSourceSelectArguments.Empty), DataView)
-            Spreadsheet.Open(EditedDocuemntID, DocumentFormat.Xlsx, Function()
-                Return CType(view.Table.Rows(0)("DocBytes"), Byte())
-            End Function)
+            Dim view As DataView = CType(SqlDataSource1.Select(DataSourceSelectArguments.Empty), DataView)
+            Spreadsheet.Open(EditedDocuemntID, DocumentFormat.Xlsx, Function() CType(view.Table.Rows(0)("DocBytes"), Byte()))
         End Sub
 
         Private Sub CloseSpreadsheetDocument()
-            DocumentManager.CloseDocument(DocumentManager.FindDocument(EditedDocuemntID).DocumentId)
+            Call DocumentManager.CloseDocument(DocumentManager.FindDocument(EditedDocuemntID).DocumentId)
             EditedDocuemntID = String.Empty
         End Sub
-        #End Region
-
+#End Region
     End Class
 End Namespace
